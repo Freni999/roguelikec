8285012426400fd0d05eb9b8b6359043c87ebd62
Freni999 <gonzalofreniche@gmail.com>
2025-07-27 18:54:46 +0000

    imorovements

--- src/roguelike.c
+++ src/roguelike.c
@@ -1,78 +1,163 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define COLS 20
 #define ROWS 10
 
-  void draw(char room[ROWS][COLS]){
-    for(int i = 0; i < ROWS;  i++){
-      for(int j = 0; j < COLS; j++){
+typedef struct {
+  int x;
+  int y;
+}coord;
+
+typedef struct {
+  int life;
+  coord pos;
+  char name[10];
+  char symbol;
+}player;
+
+typedef struct {
+  int life;
+  coord pos;
+  char symbol;
+}monster;
+
+enum keys{
+  key_up = 'w',
+  key_down = 's',
+  key_left = 'a',
+  key_right = 'd'
+};
+
+typedef enum {
+  up,
+  down,
+  left,
+  right
+}direction;
+void
+ cls(){
+  system("clear");
+}
+
+int get_random(int start , int finish){
+  srand(time(0));
+  int randoom = start + (random() % finish);
+  return randoom;
+} 
+
+void draw(char room[ROWS][COLS], player p){
+  cls();
+  for(int i = 0; i < ROWS;  i++){
+    for(int j = 0; j < COLS; j++){
+      if(i == p.pos.y && j == p.pos.x){
+          printf("%c",p.symbol);
+      }else{
         printf("%c",room[i][j]);
       }
-      puts("");
     }
-
+    puts("");
   }
 
-  void init_room(char room[ROWS][COLS]){
-    for(int i = 0; i < ROWS;  i++){
-      for(int j = 0; j < COLS; j++)
-        if(i == 0 || i == ROWS - 1 || j == 0 || j == COLS - 1){
-            room[i][j] = '#';   
-        }else{
-          room[i][j] = '.';
-        }
+}
+
+void init_room(char room[ROWS][COLS], player *p){
+  for(int i = 0; i < ROWS;  i++){
+    for(int j = 0; j < COLS; j++){
+      if(i == 0 || i == ROWS - 1 || j == 0 || j == COLS - 1){
+          room[i][j] = '#';   
+      }else{
+        room[i][j] = '.';
       }
-      room[ROWS/2][COLS/2] = '@';
-    }
-
-  void read_input(){
-    getchar();
+   }
   }
+  //p->name = "Gonzalo";
+  p->pos.y =ROWS/2;
+  p->pos.x = COLS/2;
+  p->symbol = '$';
+  //room[ROWS/2][COLS/2] = '@';
+}
 
-  void enemies(){
-
+direction read_input(){
+  char c = getchar();
+  switch(c){
+    case key_up:
+      return up;
+    break;
+    case key_down:
+    return down;
+    break;
+    case key_left:
+      return left;
+    break;
+    case key_right:
+    return right;
+    break;
   }
+  
+  
+}
 
-  void apply_rules(){
+void enemies(){
 
-  }
+}
+
+void apply_rules(){
+
+}
 
 void game_loop(){
-  char room[ROWS][COLS];
- 
+char room[ROWS][COLS];
+player hero;
 
-  init_room(room);
-  while(1){
-    draw(room);
-    read_input();
-    enemies();
-    apply_rules();
+direction d;
+init_room(room, &hero);
+while(1){
+  draw(room, hero);
+  d = read_input();
+  switch(d){
+    case up:
+    hero.pos.y = hero.pos.y-1;
+    break;
+    case down:
+    hero.pos.y = hero.pos.y+1;
+    break;
+    case left:
+    hero.pos.x = hero.pos.x-1;
+    break;
+    case right:
+    hero.pos.x = hero.pos.x+1;
+    break;
   }
+  enemies();
+  apply_rules();
+}
 }
 void game(){
-  game_loop();
+game_loop();
 }
 
 void menu(){
-  char option;
-  do {
-    puts("N - New game");
-    puts("C - Credits");
-    puts("Q - Exit");
-    scanf("%c", &option );
-    getchar();
-    switch(option){
-      case 'N':
-        game();
-      break;
-      case 'C':
+char option = 'N';
+do {
+  puts("N - New game");
+  puts("C - Credits");
+  puts("Q - Exit");
+  //scanf("%c", &option );
+  // getchar();
+  switch(option){
+    case 'N':
+      game();
+    break;
+    case 'C':
 
-      break;
-    }
-  } while(option != 'Q');
+    break;
+  }
+} while(option != 'Q');
 }
 int main(int argc, char const *argv[]) {
-  // Print a welcome message
-  printf("Welcome to the Roguelike Game!\n");
-  menu();
-  return 0;
+// Print a welcome message
+printf("Welcome to the Roguelike Game!\n");
+menu();
+return 0;
 }
